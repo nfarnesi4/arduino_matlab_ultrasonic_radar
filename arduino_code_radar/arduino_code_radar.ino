aa95ac515b0b9923ee7d7fd9074cf822b1da3e2f
Reno <nfarnesi4@gmail.com>
2016-03-02 20:30:53 -0500

    Python script woriking with arduino. Need to write matlab now

---
+++ arduino_code_radar/arduino_code_radar.ino
@@ -0,0 +1,78 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com> 
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://arduino.cc/en/Tutorial/Sweep
+*/ 
+
+#include <Servo.h> 
+#include <NewPing.h>
+
+#define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+#define SERVO_PIN 9
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+Servo servo;  // create servo object to control a servo
+
+bool new_pos_received = false;
+
+int pos = 0;
+
+String in_str = "";
+
+void setup() 
+{
+  Serial.begin(9600);
+  servo.attach(SERVO_PIN);
+  Serial.print("*"); 
+  //servo.write(pos);
+} 
+ 
+void loop() 
+{
+  
+  if(new_pos_received){
+    //move the servo
+    servo.write(pos);
+    delay(20); //wait for servo to move
+    
+    //get the distance reading in cm
+    int distance = sonar.convert_cm(sonar.ping_median());
+
+    //print the pos, distance
+    Serial.print(pos);
+    Serial.print(", ");
+    Serial.println(distance);
+
+    //wait for new data
+    new_pos_received = false;
+  }
+  
+} 
+
+//called when there is new serial data
+void serialEvent()
+{ 
+  while (Serial.available()){
+    //pos = Serial.parseInt();
+    //check for end char
+    char in = (char)Serial.read();
+    if (in != '!'){
+      in_str += in; 
+    }
+    else{
+      new_pos_received = true;
+      pos = in_str.toInt();
+      in_str = "";
+      break;
+    }
+    //char in_data = Serial.read();
+    //Serial.println(in_data);
+  }
+}
--- get_serial_data.py
+++ get_serial_data.py
@@ -3,6 +3,14 @@
 import serial, time
 import sys, signal
 
+if len(sys.argv) > 1:
+    angle = str(sys.argv[1] + '!')
+    #print('degree: ' + str(degree))
+    #print(angle)
+else:
+    sys.exit(0);
+
+
 ser = serial.Serial()
 
 ser.port = '/dev/ttyACM0'
@@ -16,7 +24,7 @@ def sigint_handler(signal, frame):
 signal.signal(signal.SIGINT, sigint_handler)
 
 
-#ser.timeout = 2 #1 for non blocking 2 for blocking
+ser.timeout = 2 #1 for non blocking 2 for blocking
 
 try:
     ser.open()
@@ -25,19 +33,33 @@ except Exception as e:
     print('Error with serial port: ' + str(e))
     exit()
 
+#data_found = false
+
 if ser.isOpen():
     try:
-        time.sleep(1)
-        ser.flushInput()
+        #flush the buffers
+        #ser.flushOutput()
         
-        while True:
-            data = ser.readline()
-            data = str(data)
-            for char in data:
-                if char in " \'\\bnr":
-                    data = data.replace(char, '')
-                    
-            print(data)
+        while True:#ser.in_watting < 1:
+            if ser.read(1) == b'*':
+                ser.flushInput()
+                break
+                #break
+
+        #send the angle that you want the distance from 
+        ser.write(angle.encode('ascii')) #angle.encode('ascii')) #angle.encode('ascii', 'replace'))
+        ser.flush()
+            
+        data = ser.readline()
+        data = str(data)
+        
+        #loop through all the characters and delete all those that are not important
+        for char in data:
+            if char in " \'\\bnr":
+                data = data.replace(char, '')
+        
+        #print the data
+        print(data)
         
     except Exception as e1:
         print('Error communicating...: ' + str(e1))
